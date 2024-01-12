c9ef77e92c8fdec6571f5e73e90246f5be120bf2
2201150 <147681633+2201150@users.noreply.github.com>
2024-01-12 10:36:43 +0900

    Revert "更新"
    This reverts commit 6d42510f73f6855ece8144b11688ab52222c22c8.

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,38 @@
-var audioPlayer = document.getElementById("audioPlayer");
+var alarm;
+alarm = new Audio();
+window.addEventListener("load", () => {
+  const f = document.getElementById("file1");
+  f.addEventListener("change", (evt) => {
+    let input = evt.target;
+    if (input.files.length == 0) {
+      return;
+    }
+    const file = input.files[0];
+    if (!file.type.match("audio.*")) {
+      alert("音声ファイルを選択してください。");
+      return;
+    }
+    const reader = new FileReader();
+    reader.onload = () => {
+      alarm.pause();
+      alarm.src = reader.result;
+    };
+    reader.readAsDataURL(file);
+  });
+});
 
 function play() {
-    audioPlayer.play();
+  alarm.play();
+  alarm.loop=false;
 }
-
 function loop() {
-    audioPlayer.loop = true;
-    audioPlayer.play();
+    alarm.play();
+    alarm.loop = true;
 }
-
 function stopp() {
-    audioPlayer.pause();
+alarm.pause();
 }
-
 function stopp2() {
-    audioPlayer.pause();
-    audioPlayer.currentTime = 0;
+alarm.pause();
+alarm.currentTime = 0;
 }
